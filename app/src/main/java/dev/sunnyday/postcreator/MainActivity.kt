ea34ae5db339ef612c7920dbf94fa1db191259d0
Aleksandr Tcikin <mail@sunnyday.dev>
2019-12-27 21:44:02 +0300

    Prepared scrollable fixed size container for post creator.

--- app/src/main/java/dev/sunnyday/postcreator/MainActivity.kt
+++ app/src/main/java/dev/sunnyday/postcreator/MainActivity.kt
@@ -2,11 +2,28 @@ package dev.sunnyday.postcreator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.updateLayoutParams
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text.layout.getLineStart(1)
+
+        updateCreatorSize()
     }
+
+    private fun updateCreatorSize() {
+        val display = resources.displayMetrics
+        val smallestSize = min(display.widthPixels, display.heightPixels)
+
+        creatorView.updateLayoutParams {
+            width = smallestSize
+        }
+    }
+    
 }
